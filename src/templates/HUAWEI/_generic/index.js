00e875715be9b10d69dddc7603d6bfbd4bf4ae16
matheuspedrow <matheuspedrow@gmail.com>
2023-08-18 15:28:01 -0300

    homolog MA5608T

--- src/templates/HUAWEI/_generic/index.js
+++ src/templates/HUAWEI/_generic/index.js
@@ -1,22 +1,26 @@
 const sshCheckStage = require('./ssh/checkStage')
 const sshDisplayBoards = require('./ssh/displayBoards')
 const sshDisplaySlots = require('./ssh/displaySlots')
+const sshDisplayPons = require('./ssh/displayPons')
+const sshDisplayOnus = require('./ssh/displayOnus')
 
 const telnetEnableRoot = require('./telnet/enableRoot')
 const telnetDisplayPermissionByUser = require('./telnet/displayPermissionByUser')
 const telnetDisplayBoard = require('./telnet/displayBoard')
-const telnetDisplayPon = require('./telnet/displayPon')
+const telnetDisplayPons = require('./telnet/displayPon')
 
 module.exports = {
   ssh: {
     checkStage: sshCheckStage,
     displayBoards: sshDisplayBoards,
     displaySlots: sshDisplaySlots,
+    displayPons: sshDisplayPons,
+    displayOnus: sshDisplayOnus,
   },
   telnet: {
     enableRoot: telnetEnableRoot,
     displayPermissionByUser: telnetDisplayPermissionByUser,
     displayBoard: telnetDisplayBoard,
-    displayPon: telnetDisplayPon,
+    displayPon: telnetDisplayPons,
   }
 }
---
+++ src/templates/HUAWEI/_generic/ssh/displayPons.js
@@ -0,0 +1,65 @@
+const { connect } = require('../../../../config/ssh-connect')
+const { dummy2json } = require('../../../../utils/lib')
+
+/*
+  -------------------------------------------------------------
+    Port   Port   min-distance   max-distance   Optical-module
+           type       (km)           (km)           status
+  -------------------------------------------------------------
+    0     GPON        0              20             Online
+    1     GPON        0              20             Online
+    2     GPON        0              20             Online
+    3     GPON        0              20             Online
+    4     GPON        0              20             Online
+    5     GPON        0              20             Online
+    6     GPON        0              20             Online
+    7     GPON        0              20             Online
+  -------------------------------------------------------------
+*/
+
+const displayPons = async ({ params, ...options }, { board: boardNumber, pon }) => {
+  const { numberPort } = params // VALIDAR NECESSIDADE
+
+  const cmd = `display board ${boardNumber}/${pon}}`
+  const conn = await connect(options)
+  const chunk = await conn.exec7(cmd)
+  if (!chunk && chunk === '') return null
+  
+  const splitted = chunk.split('\r\n')
+  splitted.shift()
+  splitted.shift()
+  splitted.pop()
+
+  const columns = [
+    [0, 10],
+    [12, 23],
+    [24, 35],
+    [36, 46],
+    [48, 64],
+  ]
+  
+  const data = dummy2json(splitted.join('\n'), columns, 2)
+  let min_range = '0'
+  const max_range = String(min_range || '')
+
+  return data
+    .map((item) =>  {
+      const hasIndex = (item.olt || '').toLowerCase().indexOf('pon') > -1
+      const [type_port] = hasIndex ? (item.olt || '').split('pon') : ['G']
+      const type_port_default = (`${type_port || 'g'}pon`).toLowerCase()
+        .toLowerCase()
+        .replace('km', '')
+        .trim()
+        .split('/')
+
+      return {
+      type: type_port_default,
+      min_range: 0,
+      max_range: parseInt(max_range, 10) * 1000,
+      operational_status: item.status === 'Online' ? 'Online' : 'Offline',
+      description: '',
+      }
+    })
+}
+
+module.exports = displayPons
