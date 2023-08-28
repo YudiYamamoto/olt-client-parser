7b7e10120f0ebe2469191832e4931de665b2b4b8
Thiago Zampieri <thiago.zampieri@gmail.com>
2023-08-28 12:23:37 -0300

    fix homo limit of onus by pon

--- src/templates/MADE4OLT/_generic/ssh/displayOnus.js
+++ src/templates/MADE4OLT/_generic/ssh/displayOnus.js
@@ -2,16 +2,18 @@ const displayOnu = require('./displayOnu')
 const chance = require('chance').Chance()
 
 const displayOnus = async (options, params) => {
-  const { size = 128 } = options && options.__extra__ && options.__extra__.onu || {}
+  const { onus_count = 0 } = (params && params.custom_fields) || {}
+  const { size = 128 } = (options && options.__extra__ && options.__extra__.onu) || {}
   
-  const length = chance.integer({ min: 0, max: size })
-  const data = []
-  for await (const [index] of Array.from({ length }).entries()) {
-    const ont_id = index + 1
-    const onu = await displayOnu(options, { ...params, ont_id })
-    if (onu) data.push(onu)
-  }
-      
+  const data = []  
+  if (onus_count === 0) {
+    const length = chance.integer({ min: 0, max: size })
+    for await (const [index] of Array.from({ length }).entries()) {
+      const ont_id = index + 1
+      const onu = await displayOnu(options, { ...params, ont_id })
+      if (onu) data.push(onu)
+    }
+  }      
   return data
 }
 
