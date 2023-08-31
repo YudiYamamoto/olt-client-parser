1247f444da59750c7f652c100b3284a8c51b0772
Thiago Zampieri <thiago.zampieri@gmail.com>
2023-08-31 14:06:54 -0300

    new version

--- src/templates/MADE4OLT/_generic/ssh/displayOnus.js
+++ src/templates/MADE4OLT/_generic/ssh/displayOnus.js
@@ -2,17 +2,17 @@ const displayOnu = require('./displayOnu')
 const chance = require('chance').Chance()
 
 const displayOnus = async (options, params) => {
-  const { onus_count = 0 } = (params && params.custom_fields) || {}
-  const { size = 128 } = (options && options.__extra__ && options.__extra__.onu) || {}
+  const { board = '1', slot = '1', port = '1' } = params || {};
+  // const { onus_count = 0 } = (params && params.custom_fields) || {}
+  const { size = 99 } = (options && options.__extra__ && options.__extra__.onu) || {}
 
   const data = []  
-  if (parseInt(onus_count, 10) === 0) {
-    const length = chance.integer({ min: 0, max: size })
-    for await (const [index] of Array.from({ length }).entries()) {
-      const ont_id = index + 1
-      const onu = await displayOnu(options, { ...params, ont_id })
-      if (onu) data.push(onu)
-    }
+  const length = chance.integer({ min: 0, max: size })
+  for await (const [index] of Array.from({ length }).entries()) {
+    const ont_id = index + 1
+    const mac_address = `FF:${board.padStart(2, '0')}:${slot.padStart(2, '0')}:${port.padStart(2, '0')}:MM:${ont_id.toString().padStart(2, '0')}`
+    const onu = await displayOnu(options, { ...params, ont_id, mac_address })
+    if (onu) data.push(onu)
   }      
   return data
 }
