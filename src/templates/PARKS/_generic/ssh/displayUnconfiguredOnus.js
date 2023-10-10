b7e17341a140082df70a7682b018289d42623df8
Bruno Yamamoto <yamamoto.yudi@made4it.com.br>
2023-10-10 10:53:09 -0300

    adjusting codingstyle

--- src/templates/PARKS/_generic/ssh/displayUnconfiguredOnus.js
+++ src/templates/PARKS/_generic/ssh/displayUnconfiguredOnus.js
@@ -20,38 +20,22 @@ module.exports = async (options) => {
 	if (!response) return null;
 
 	const output = response.replace(/[|+-]/g, '');
-	//SAIDA com replace
-	/*
-		10.12.13.2: terminal length 0
-		PARKS#show gpon onu unconfigured
-		Interface        Serial        Model                         
-			
-		gpon1/1          prks00b80c94  Fiberlink 1100B (SFU) Rev 2   
-		gpon1/1          prks00ce1e95  FiberLink101                  
-		gpon1/1          prks00ce1e94  FiberLink101                  
-		PARKS#
-	*/
 
 	const splitted = output.split('\r');
-	splitted.shift(); // remove: 10.12.13.2: terminal length 0
-	splitted.shift(); // remove: PARKS#show gpon onu unconfigured
-	//Content
-	splitted.pop(); // remove: PARKS#
+	splitted.shift();
+	splitted.shift();
+	splitted.pop();
 
 	const columns = [
-    [0, 17],  // interface
-    [17, 31], // serial
-    [31, 70], // model
+    [0, 17],
+    [17, 31],
+    [31, 70],
   ]
 
 	const data = dummy2json(splitted.join('\n'), columns, 1)
 	const regex = /^(?<pon_type>gpon)(?<board>\d+?)?(?:\/|\.?)?(?<slot>\d+?)?$/;
-	
-	// console.log(data);
-	// return;
 	return data.map((item) => {
 		let interface = item.interface.match(regex).groups;
-		console.log(item);
 
 		return {
       pon_type: interface.pon_type,
